Sagnac/OpticalRayTracing.jl

commit 79f688daa710da3c8250859680f7c25f9db234cf

`rayplot`: align refractive indices to the number of lens elements
`construct` can drop the last surface so this ensures the dimensions
match.

src/OpticalRayTracing.jl
@@ -304,14 +304,14 @@ function rayplot(n::AbstractVector, lens::Lens, a::AbstractVector, h′ = -0.5)
 end
 
 function rayplot(surfaces::Matrix{Float64}, system::System)
-    n = @view surfaces[:,3]
     lens = construct(surfaces)
+    n = @view surfaces[begin:size(lens.A, 1), 3]
     rayplot(n, lens, system)
 end
 
 function rayplot(surfaces::Matrix{Float64}, a::AbstractVector, h′ = -0.5)
-    n = @view surfaces[:,3]
     lens = construct(surfaces)
+    n = @view surfaces[begin:size(lens.A, 1), 3]
     system = solve(lens, a, h′)
     rayplot(n, lens, system)
 end

commit 6922a5fc0d13078cfc0b1cb607f4f64f5705fae9

`rayplot`: fix selection of refractive indices
Commit 79f688d introduced this bug.

src/OpticalRayTracing.jl
@@ -244,7 +244,7 @@ end
 function raypoints(n::AbstractVector, lens::Lens, system::System)
     (; A) = lens
     (; marginal, chief) = system.rays
-    t = @view(A[:,1]) .* n
+    t = map(*, @view(A[:,1]), @view n[begin:end-1])
     k = length(t) + 2
     l = sum(t)
     s = 0.1
@@ -304,14 +304,14 @@ function rayplot(n::AbstractVector, lens::Lens, a::AbstractVector, h′ = -0.5)
 end
 
 function rayplot(surfaces::Matrix{Float64}, system::System)
+    n = @view surfaces[:,3]
     lens = construct(surfaces)
-    n = @view surfaces[begin:size(lens.A, 1), 3]
     rayplot(n, lens, system)
 end
 
 function rayplot(surfaces::Matrix{Float64}, a::AbstractVector, h′ = -0.5)
+    n = @view surfaces[:,3]
     lens = construct(surfaces)
-    n = @view surfaces[begin:size(lens.A, 1), 3]
     system = solve(lens, a, h′)
     rayplot(n, lens, system)
 end