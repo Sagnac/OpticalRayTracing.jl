6a15752be5e6a5ae619523a6cc3a13187a6a5d6a
Sagnac <83491030+Sagnac@users.noreply.github.com>
2025-12-03 05:07:40 +0000

    Optimization: reassign minimized values before updating
    I completely missed this. This means that each update was using the
    previous iteration's values so this should actually improve results.
    
    Also, a superfluous variable probably left over from one of the early
    design iterations has been deleted and the Lagrangian now has an
    exclamation mark to denote that it actually mutates the layout since it
    calls the update! function. What is shadowed has also been adjusted for
    consistency and some names changed for clarity.

--- src/Optimization.jl
+++ src/Optimization.jl
@@ -25,40 +25,39 @@ function optimize(surfaces, system, v, constraints,
     # penalty
     μ = Ref{Float64}(10.0) # since closed over and mutated, this avoids Core.Box
     # init
-    x0 = x = layout[v]
+    x0 = layout[v]
     x0[isinf.(x0)] .= 1e7
     # toleration / patience counter for increasing multipliers
     k = 0
     # monitor constraint satisfaction
-    cᵢ = get_constraints(system)
-    local new_system
+    c_prev = get_constraints(system)
     function update!(x)
         layout[v] .= x
-        sys = solve(layout, a, h′)
-        c = get_constraints(sys)
-        return sys, c
+        local system = solve(layout, a, h′)
+        local c = get_constraints(system)
+        return system, c
     end
     for i = 1:24
         # Lagrangian / objective function
-        function L(x)
-            sys, c = update!(x)
-            W = aberrations(layout, sys)
+        function L!(x)
+            local system, c = update!(x)
+            W = aberrations(layout, system)
             Wᵢ = abs.(getfield(W, i) for i ∈ aberr)
             return Wᵢ ⋅ weights / ∑wts + 0.5 * μ[] * c ⋅ c + λ ⋅ c
         end
-        result = Optim.optimize(L, x0)
-        new_system, cᵢ′ = update!(x)
-        λ .*= μ[] .* cᵢ′
+        result = Optim.optimize(L!, x0)
+        x0 = Optim.minimizer(result)
+        system, c = update!(x0)
+        λ .*= μ[] .* c
         # update the penalty parameter if residuals stagnate
         # or multipliers start blowing up
-        if norm(cᵢ′) > 0.8 * norm(cᵢ) || k == 3
+        if norm(c) > 0.8 * norm(c_prev) || k == 3
             μ[] *= 10.0
             k = 0
         elseif norm(λ) > 10.0 * μ[]
             k += 1
         end
-        cᵢ = cᵢ′
-        x = x0 = Optim.minimizer(result)
+        c_prev = c
     end
-    return layout, new_system
+    return layout, system
 end
